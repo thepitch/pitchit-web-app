600de067db461e963860af37f4345db07fad8170
Stephanie Hutson <email": null>
2015-10-15 10:12:16 -0700

    get the signup info to pass to the other server

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
     event.preventDefault();
     console.log("Fade city")
     fadeInSignUp();
-  }); 
-
+  });
+  $("body").on("submit", '#header-signup-form', sendUserInfo)
 
   $(".overlay").click(function(event){
     event.preventDefault();
@@ -25,4 +25,20 @@ var fadeInSignUp = function(){
 var fadeOutSignUp = function(){
   $(".sign-up-box").fadeOut("fast");
   $(".overlay").fadeOut("fast");
+}
+
+var sendUserInfo = function(e){
+  e.preventDefault()
+  // console.log("HIT!")
+  var data = $(this).serialize()
+  var url = 'http://localhost:3000/users'
+  var method = $(this).attr("method")
+  $.ajax({
+    url: url,
+    data: data,
+    method: "post",
+    dataType: "json"
+  }).done(function(reseponse){
+    console.log(response)
+  })
 }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 require 'ap'
+require 'json'
+require "httparty"
 
 class UsersController < ApplicationController
 
@@ -6,7 +8,6 @@ class UsersController < ApplicationController
     id = params[:id]
 
     response = HTTParty.get('http://localhost:3000/users/'+id)
-    ap response
     @user = response
   end
 
@@ -16,10 +17,10 @@ class UsersController < ApplicationController
 
   def create
     ap params
-    ap params[:user]
-    HTTParty.post('http://localhost:3000/users',
-      {user: params}
+    @result = HTTParty.post('http://localhost:3000/users'.to_s,
+      {user: params.to_json}
     )
+    # ap @result
     redirect_to '/pitches'
   end
 
