6a98a257989943c2cff1d9c3749d1256199f3cbb
kkennis <kevinvkennis@gmail.com>
2015-10-15 15:21:13 -0700

    Complete login

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -7,23 +7,23 @@ $(document).ready(function(){
   });
 
 
-  $("header").on("submit", "#header-login-form", function(event){
-    event.preventDefault();
-    var action = 'http://localhost:3000/users/login';
-    var method = $(this).attr("method");
-    var data = $(this).serialize();
-    var currentUrl = document.URL
-    $.ajax({
-      url: action,
-      method: method,
-      dataType: 'json',
-      data: data
-    })
-    .done(function(response){
-      console.log("HIT")
-      window.location.replace("/users/" + response.id)
-    });
-  });
+//   $("header").on("submit", "#header-login-form", function(event){
+//     event.preventDefault();
+//     var action = 'http://localhost:3000/users/login';
+//     var method = $(this).attr("method");
+//     var data = $(this).serialize();
+//     var currentUrl = document.URL
+//     $.ajax({
+//       url: action,
+//       method: method,
+//       dataType: 'json',
+//       data: data
+//     })
+//     .done(function(response){
+//       console.log("HIT")
+//       window.location.replace("/users/" + response.id)
+//     });
+//   });
 
   $(".exit-login").click(function(event){
     $("#header-login-form").hide();
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,30 @@
 class SessionsController < ApplicationController
+  include UsersHelper
+
   def new
   end
 
   def create
-    response = RestClient.post 'http://localhost:3000/users', :user => params, :accept => :json
+    p "LOGIN PARAMS"
+    p "*" * 80
+    p params
+    p "*" * 80
 
-    if response
+
+    response = RestClient.post 'http://localhost:3000/users/login', :user => params, :accept => :json
+
+
+    unless response == "Invalid Login"
+      response = JSON.parse(response)
+    end
+
+
+    if response != "Invalid Login"
+      session[:user_id] = response["id"]
       @current_user = get_current_user
-      redirect_to user_path(@current_user)
+      redirect_to user_path(@current_user["id"])
+    else
+      redirect_to root_path
     end
   end
 
@@ -15,4 +32,23 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_path
   end
+
+  private
+    def set_current_user
+      @current_user = get_current_user
+    end
+
+    def get_current_user
+      return nil unless session[:user_id]
+
+
+      current_user = RestClient.get("http://localhost:3000/users/" + session[:user_id].to_s, :accept => :json)
+
+      p "Current User*" * 20
+      p JSON.parse(current_user)
+      p "Current User*" * 20
+
+      JSON.parse(current_user)
+    end
 end
+  
