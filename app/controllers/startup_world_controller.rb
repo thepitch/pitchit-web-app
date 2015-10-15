1957cb48199487aa69d8e7e75de16838dfeeef45
kkennis <kevinvkennis@gmail.com>
2015-10-14 19:13:13 -0700

    Add pitch of the week

--- app/controllers/startup_world_controller.rb
+++ app/controllers/startup_world_controller.rb
@@ -1,12 +1,13 @@
 require "date"
+
+
 class StartupWorldController < ApplicationController
   def index
-    # if current_user
-    #   @user_type = current_user.user_type
-    # else
-    #   @user_type = "guest"
-    # end
+    response = HTTParty.get('http://localhost:3000/pitch_of_the_week').parsed_response
+
+    p response
+
     @user_type = "guest"
-    # @pitch_of_the_week = Pitch.where("created_at > :week", {week: 1.week.ago }).sort_pitches("score").first
+    @pitch_of_the_week = response
   end
 end
