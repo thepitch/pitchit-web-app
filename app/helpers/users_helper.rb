4aad19ac732776f4c146a731bcc74d6c567b019f
Moses Jung <MosesJung86@gmail.com>
2015-10-16 11:44:19 -0700

    got rid of p in users_helper file

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,14 +2,8 @@ module UsersHelper
 
   def get_current_user
     return nil unless session[:user_id]
-    p "$$$$$$$$$$$$$$$$$"
-    p session[:user_id]
-    p "$$$$$$$$$$$$$$$$$"
-    current_user = RestClient.get("http://localhost:3000/users/" + session[:user_id].to_s, :accept => :json)
 
-    p "Current User*" * 20
-    p JSON.parse(current_user)
-    p "Current User*" * 20
+    current_user = RestClient.get("http://localhost:3000/users/" + session[:user_id].to_s, :accept => :json)
 
     JSON.parse(current_user)
   end
@@ -18,10 +12,7 @@ module UsersHelper
     if get_current_user
       false
       user_votes = get_current_user["votes"]
-      p "*"*70
-      p votable_id
-      p votable_type
-      p "*"*70
+
       user_votes.each do |vote|
         if vote["votable_type"] == votable_type && vote["votable_id"] == votable_id.to_i
         end
